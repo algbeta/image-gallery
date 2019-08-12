32b9c22e6756e78464bd3d48c1ce2bb3f4d5e622
Natallia <natallia_gorbunova@epam.com>
2019-08-12 04:34:17 +0200

    remove unused data

--- src/state/api.js
+++ src/state/api.js
@@ -1,27 +1,7 @@
-import 'whatwg-fetch';
-import data from '../data/images.json'
+import data from '../data/images.json';
 
-const apiGetStatic =  () => {
+const apiGetStatic = () => {
   return data;
-}
-
-const baseFetchApi = (options, method) => {
-  const { url, data } = options;
-  return fetch(url, {
-    method,
-    body: data,
-  })
-    .catch(error => {
-      if (error && !error.response) {
-        throw new Error('Failed to fetch');
-      } else {
-        return Promise.reject(error);
-      }
-    })
-    .then(response => response.json())
-    .catch(error => Promise.reject(error));
 };
-const api = options => baseFetchApi(options, 'POST');
-const apiGet = options => baseFetchApi(options, 'GET');
 
-export { baseFetchApi, api, apiGet, apiGetStatic };
+export { apiGetStatic };
